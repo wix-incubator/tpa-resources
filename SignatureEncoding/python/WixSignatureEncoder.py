ed3a6f8d1b9968f997e5fbf803e8eab21e8776e9
Gil Tayar <gilt@wix.com>
2014-02-25 07:01:01 +0200

    reordered functions and added comments

--- SignatureEncoding/python/WixSignatureEncoder.py
+++ SignatureEncoding/python/WixSignatureEncoder.py
@@ -12,13 +12,6 @@ API_PORT = 443
 API_VERSION = '1.0.0'
 
 
-def create_http_headers(app_key, instance_id):
-    return {
-        'x-wix-application-id': app_key,
-        'x-wix-instance-id': instance_id,
-        'x-wix-timestamp': datetime.now(pytz.utc).isoformat(),
-    }
-
 
 def add_signature_header(http_headers, signature):
     http_headers['x-wix-signature'] = signature
@@ -44,6 +37,28 @@ def remove_padding(signature):
     return signature.replace('=', '')
 
 
+
+def add_parameters_to_path(path, parameters):
+    return path + "?" + \
+        urllib.urlencode(dict(itertools.chain(parameters.iteritems(), [('version', API_VERSION)])))
+
+
+#
+# This function generates the list of headers that are to be sent via HTTP to the api
+#
+def create_http_headers(app_key, instance_id):
+    return {
+        'x-wix-application-id': app_key,
+        'x-wix-instance-id': instance_id,
+        'x-wix-timestamp': datetime.now(pytz.utc).isoformat(),
+    }
+
+
+#
+# This is the function that does the calculation of the signature
+# Note that the http headers it accepts is the list created by the above 'create_headers'
+# function
+#
 def calculate_signature(method, path, parameters, body, http_headers, secret_key):
     to_sign = generate_string_to_be_signed(method, path, parameters, body, http_headers)
 
@@ -56,11 +71,11 @@ def calculate_signature(method, path, parameters, body, http_headers, secret_key
     return encoded_without_padding
 
 
-def add_parameters_to_path(path, parameters):
-    return path + "?" + \
-        urllib.urlencode(dict(itertools.chain(parameters.iteritems(), [('version', API_VERSION)])))
-
-
+#
+# Use this function to create the http headers (including the signature header)
+# that is to be sent via HTTP in the api call. The function also returns
+# the modified path that is to be used in the HTTP call
+#
 def create_http_data(app_key, secret_key, instance_id, path, parameters, body, method):
     http_headers = create_http_headers(app_key, instance_id)
     signature = calculate_signature(method, path, parameters, body, http_headers, secret_key)
